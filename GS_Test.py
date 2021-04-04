3ee616afc8c1f5f8073a3d9dab7988d8e1ad4209
Hansen-Erik <Hanse109@rangers.uwp.edu>
2021-04-03 19:52:10 -0500

    Copied code

---
+++ GS_Test.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Borrowed Code
+
+def modifiedGramSchmidt(A):
+    """
+    Gives a orthonormal matrix, using modified Gram Schmidt Procedure
+    :param A: a matrix of column vectors
+    :return: a matrix of orthonormal column vectors
+    """
+    # assuming A is a square matrix
+    dim = A.shape[0]
+    Q = np.zeros(A.shape, dtype=A.dtype)
+    for j in range(0, dim):
+        q = A[:, j]
+        for i in range(0, j):
+            rij = np.vdot(Q[:, i], q)
+            q = q - rij * Q[:, i]
+        rjj = np.linalg.norm(q, ord=2)
+        if np.isclose(rjj, 0.0):
+            raise ValueError("invalid input matrix")
+        else:
+            Q[:, j] = q / rjj
+    return Q
+
+
+
+
